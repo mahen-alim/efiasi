e745c656e2011edfb5785163e9cd1d3158faa472
Mahen <mahennekkers27@gmail.com>
2024-09-03 21:13:30 +0700

    student responsive form

--- app/Http/Controllers/userApiController.php
+++ app/Http/Controllers/userApiController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class userApiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $users = User::all();
 
